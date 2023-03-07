6e429586619f82867efa6fb262991cdec36ae3b5
Samuel Pérez <shamuperez93@gmail.com>
2023-03-06 21:09:37 -0500

    JOI y variables de entorno

--- README.md
+++ README.md
@@ -17,6 +17,16 @@ npm i -g @nest/cli
 ```
 docker-compose up -d
 ```
+5. Clonar el archivo __.env.template__ y renombrar la copia a __.env__
+6. LLenar las variables de entorno definidas en el __.env__
+7. Ejecutar la aplicación en dev: 
+```
+yarn start:dev
+```
+8. Reconstruir las base de datos con SEED (desarrollo).
+```
+http://localhost:3000/api/v2/seed
+```
 
 ## Stack usado
 * MongoDB
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,48 @@
+import  { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter
+  ) { }
+
+  
+  
+  async execute() {
+    await this.pokemonModel.deleteMany({});
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    // Insertar multiples registros simultaneamente
+    // const insertPromiseArray = [];
+    // data.results.forEach(({name, url}) => {
+    //   const segments = url.split('/');
+    //   const no: number = +segments[ segments.length - 2];
+    //   insertPromiseArray.push(this.pokemonModel.create({ name, no}))
+    // })
+    // await Promise.all(insertPromiseArray);
+
+    // Segunda menara de insertar y la más óptima
+    const pokemonToInsert: { name: string, no: number}[] = [];
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const no: number = +segments[ segments.length - 2];
+      pokemonToInsert.push({ name, no})
+    })
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+
+
+
+    return 'Seed executed';
+  }
+
+}
